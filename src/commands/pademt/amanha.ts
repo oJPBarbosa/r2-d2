d64ca10bca0f5b50f5458d1b7f2cbd79b86ac15c
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-04-10 20:08:10 -0300

    fix(amanha.ts): address weekday logic issue

--- src/commands/pademt/amanha.ts
+++ src/commands/pademt/amanha.ts
@@ -11,7 +11,7 @@ export default {
   async execute(message: Message): Promise<void> {
     const tutorings: TutoringT[] = Schedules.getDayTutorings(
       message.guild.id,
-      weekdays[date().weekday + 1 > 6 ? 0 : date().weekday + 1],
+      weekdays[date().weekday + 1 > 7 ? 0 : date().weekday + 1],
     );
 
     if (!tutorings) {
