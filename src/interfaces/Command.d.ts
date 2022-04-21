f320a39b01d5c572ed5880d62614a290e5d1a862
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-04-21 19:41:00 -0300

    chore(Command.d.ts): add missing T to CommandData

--- src/interfaces/Command.d.ts
+++ src/interfaces/Command.d.ts
@@ -1,11 +1,11 @@
 import { Message } from 'discord.js';
 
-export type CommandData = {
+export type CommandDataT = {
   name: string;
   description: string;
 };
 
 export type CommandT = {
-  data: CommandData;
+  data: CommandDataT;
   execute(message: Message): Promise<Message<boolean>>;
 };
